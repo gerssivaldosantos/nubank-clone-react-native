0f439b8b5864ceb1739d2cf7ef08a010308d50f7
andows159 <canalandows@gmail.com>
2021-04-08 09:02:49 -0300

    Account card finalized and footer initialized

--- src/screens/home.js
+++ src/screens/home.js
@@ -7,7 +7,7 @@ import {
   SafeAreaView,
   View,
   TouchableOpacity,
-  Touchable,
+  ScrollView,
   Dimensions,
 } from "react-native";
 
@@ -17,7 +17,7 @@ export default function App() {
   const usuario = "Gerssivaldo";
   const faturaAtual = "R$ 5,31";
   const saldoDisponivel = "R$ 8,90";
-  const limite = 'R$ 48,76'
+  const limite = "R$ 48,76";
   const [isHidde, setisHidde] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
@@ -52,7 +52,11 @@ export default function App() {
         </TouchableOpacity>
       </View>
 
-      <View>
+      <ScrollView contentContainerStyle={{
+        alignItems: 'center',
+
+        }} style={styles.scrollContainer}>
+          
         <View style={styles.cardContainer}>
           <Ionicons style={styles.cardIcon} size={29} name="ios-card-outline" />
 
@@ -63,7 +67,94 @@ export default function App() {
           <Text style={styles.limitTitle}>Limite disponível</Text>
           <Text style={styles.limit}>{limite}</Text>
         </View>
-      </View>
+
+        <View style={styles.accountContainer}>
+          <Ionicons style={styles.accountIcon} name="ios-cash-outline" size={26}/>
+          <Text style={styles.accountTitle}>
+            Conta
+          </Text>
+          <Text style={styles.accountSubTitle}>
+            Saldo disponível
+          </Text>
+          <Text style={styles.cash}>
+            {saldoDisponivel}
+          </Text>
+
+        </View>
+        <View style={styles.cardContainer}>
+          <Ionicons style={styles.cardIcon} size={29} name="ios-card-outline" />
+
+          <Text style={styles.cardTitle}> Cartão de Crédito </Text>
+
+          <Text style={styles.cardSubTitle}> Fatura atual</Text>
+          <Text style={styles.invoiceTitle}>{faturaAtual}</Text>
+          <Text style={styles.limitTitle}>Limite disponível</Text>
+          <Text style={styles.limit}>{limite}</Text>
+        </View>
+
+        <View style={styles.accountContainer}>
+          <Ionicons style={styles.accountIcon} name="ios-cash-outline" size={26}/>
+          <Text style={styles.accountTitle}>
+            Conta
+          </Text>
+          <Text style={styles.accountSubTitle}>
+            Saldo disponível
+          </Text>
+          <Text style={styles.cash}>
+            {saldoDisponivel}
+          </Text>
+
+        </View>
+        <View style={styles.cardContainer}>
+          <Ionicons style={styles.cardIcon} size={29} name="ios-card-outline" />
+
+          <Text style={styles.cardTitle}> Cartão de Crédito </Text>
+
+          <Text style={styles.cardSubTitle}> Fatura atual</Text>
+          <Text style={styles.invoiceTitle}>{faturaAtual}</Text>
+          <Text style={styles.limitTitle}>Limite disponível</Text>
+          <Text style={styles.limit}>{limite}</Text>
+        </View>
+
+        <View style={styles.accountContainer}>
+          <Ionicons style={styles.accountIcon} name="ios-cash-outline" size={26}/>
+          <Text style={styles.accountTitle}>
+            Conta
+          </Text>
+          <Text style={styles.accountSubTitle}>
+            Saldo disponível
+          </Text>
+          <Text style={styles.cash}>
+            {saldoDisponivel}
+          </Text>
+
+        </View>
+      </ScrollView>
+      <ScrollView horizontal={true} style={styles.footer}>
+        
+        <View style={styles.childFooter}>
+
+        </View>
+        <View style={styles.childFooter}>
+
+        </View>
+        <View style={styles.childFooter}>
+
+        </View>
+        <View style={styles.childFooter}>
+
+        </View>
+        <View style={styles.childFooter}>
+
+        </View>
+        <View style={styles.childFooter}>
+
+        </View>
+        <View style={styles.childFooter}>
+
+        </View>
+       
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -76,7 +167,6 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   header: {
-    /* backgroundColor:'green', */
     width: "100%",
     height: "12%",
     alignItems: "center",
@@ -92,40 +182,44 @@ const styles = StyleSheet.create({
   },
   buttonHidde: {
     position: "absolute",
-    width: 52,
-    height: 52,
-    backgroundColor: "#ffffff20",
+    width: 48,
+    height: 48,
+    backgroundColor: "#b0b0b030",
     justifyContent: "center",
     alignItems: "center",
     right: "20%",
     borderRadius: 50,
-  
   },
   buttonSettings: {
     position: "absolute",
-    width: 52,
-    height: 52,
-    backgroundColor: "#ffffff20",
+    width: 48,
+    height: 48,
+    backgroundColor: "#b0b0b030",
     justifyContent: "center",
     alignItems: "center",
     right: "5%",
     borderRadius: 50,
-  
+  },
+  scrollContainer:{
+    width:'100%',
+    
+    
   },
   cardContainer: {
-    height: deviceHeight * 0.2,
+    height: 165,
+    marginBottom:18,
     backgroundColor: "white",
-    width: deviceWidth * 0.9,
+    width: deviceWidth - 38,
     borderRadius: 2,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 1,
     },
     shadowOpacity: 0.1,
-    shadowRadius: 1.84,
+    shadowRadius: 1,
 
-    elevation: 3,
+    elevation: 1,
   },
   cardTitle: {
     position: "absolute",
@@ -140,34 +234,93 @@ const styles = StyleSheet.create({
     top: 22,
     color: "#828282",
   },
-  cardSubTitle:{
+  cardSubTitle: {
     position: "absolute",
     left: 22,
     top: 64,
     fontSize: 13,
     color: "#828282",
   },
-  invoiceTitle:{
+  invoiceTitle: {
     position: "absolute",
     left: 26.5,
-    fontWeight:'bold',
+    fontWeight: "bold",
     top: 90,
     fontSize: 25,
     color: "#1fa9de",
   },
-  limitTitle:{
+  limitTitle: {
     position: "absolute",
     left: 28,
-    top: '76%',
+    top: "76%",
     fontSize: 13,
     color: "#454545",
   },
-  limit:{
+  limit: {
     position: "absolute",
     left: 140,
-    top: '76%',
+    top: "76%",
     fontSize: 13,
     color: "green",
-    fontWeight:'bold'
+    fontWeight: "bold",
+  },
+  accountContainer:{
+    height: 150,
+    marginBottom:18,
+    backgroundColor: "white",
+    width: deviceWidth - 38,
+    borderRadius: 2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+
+    elevation: 1,
+  },
+  accountTitle:{
+    position: "absolute",
+    left: 72,
+    top: 25,
+    fontSize: 15,
+    color: "#828282",
+  },
+  accountIcon:{
+      position: "absolute",
+      left: 24,
+      top: 22,
+      color: "#828282",
+  },
+  accountSubTitle:{
+    position: "absolute",
+    left: 27,
+    top: 64,
+    fontSize: 13,
+    color: "#828282",
+  },
+  cash:{
+    position: "absolute",
+    left: 26.5,
+    fontWeight: "bold",
+    top: 90,
+    fontSize: 25,
+    color: "#232323",
+  },
+  footer:{
+    height:200,
+    paddingTop:10,
+    flexDirection:'row',
+    paddingLeft:20
+  },
+  childFooter:{
+    marginTop:7,
+    marginRight:10,
+    backgroundColor:'#ffffff25',
+    height:90,
+    width:90,
+    
+
   }
 });
