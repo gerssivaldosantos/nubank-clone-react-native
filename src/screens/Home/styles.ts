19bf2240b84c1330fd85deae77eeac5b742d21b0
gerssivaldosantos <canalandows@gmail.com>
2024-05-19 06:53:01 -0300

    feat: add Home

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,10 +1,15 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   background-color: ${({ theme }) => theme.background};
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   width: 100%;
+  height: 100%;
+`;
+
+export const TransparentButton = styled.TouchableOpacity`
+  background-color: transparent;
 `;
