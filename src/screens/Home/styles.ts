c734add7b97da2929c0aae83b3263943fa9fdf0c
gerssivaldosantos <canalandows@gmail.com>
2024-05-19 21:31:19 -0300

    feat: add Icons

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
-
+import { FontAwesome6 } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 export const Container = styled.SafeAreaView`
   background-color: ${({ theme }) => theme.background};
   display: flex;
@@ -13,3 +14,19 @@ export const Container = styled.SafeAreaView`
 export const TransparentButton = styled.TouchableOpacity`
   background-color: transparent;
 `;
+
+export const IconFontAweasome6 = styled(FontAwesome6)`
+  color: ${({ theme }) => theme.text};
+`;
+
+export const IconFeather = styled(Feather)`
+  color: ${({ theme }) => theme.text};
+`;
+
+export const PixIcon = styled(IconFontAweasome6).attrs({ name: 'pix', size: 24 })``;
+
+export const BarcodeIcon = styled(IconFontAweasome6).attrs({ name: 'barcode', size: 24 })``;
+
+export const MoneyBillIcon = styled(IconFontAweasome6).attrs({ name: 'money-bill-1', size: 24 })``;
+
+export const SmartphoneIcon = styled(IconFeather).attrs({ name: 'smartphone', size: 24 })``;
